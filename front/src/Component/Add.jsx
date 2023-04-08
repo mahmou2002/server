11d13a1916f5ed19db01e244f6c2d9bc36539ac0
mahmoud <121032812+mahmou2002@users.noreply.github.com>
2023-04-08 08:01:38 +0100

    set

--- front/src/Component/Add.jsx
+++ front/src/Component/Add.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
-
-const Add = () => {
+import React, { useState } from 'react'
+import axios from 'axios'
+const add= ({data}) => {
+ // const[namee,setNamee]=useState("")
+ //const [description,setDescription]=useState("")
+ //const[price,setPrice]=useState("")
+ //const[imageUrl,setImageurl]=useState("")
+ //const[category,setCategory]=useState("")
+ //const[data,setdata]=useState([])
+  //const submit =()=>{
+    //axios.post("http://localhost:5002/api/product/update"+{data.id},{namee,description,price,imageUrl,category}).then((res)=>{
+      //  setdata(res.data)
+       
+    //}).catch((err)=>{
+      //  console.log(err);
+    //})
+   
+//}
   return (
-    <div>Add</div>
+    <div className='update'>
+      <ul>
+       <input  type="text" placeholder='name'   />
+       <input type="text" placeholder='description'    />
+       <input type="text" placeholder='price'    />
+       <input type="text" placeholder='imageUrl'   />
+       <input type="text" placeholder='category'   />
+        <input type="button" value="submit" />
+
+      </ul>
+    </div>
   )
 }
 
-export default Add
+export default add
+
+//import React, { useState } from 'react'
+//import axios from 'axios'
+//const Update = ({id}) => {
+  //[namee,setNamee]=useState("")
+ // [description,setDescription]=useState("")
+  //[price,setPrice]=useState("")
+  //[imageUrl,setImageurl]=useState("")
+  //[category,setCategory]=useState("")
+
+  //const submit =()=>{
+   //axios.post("http://localhost:5002/api/product/update"+{data.id},{namee,description,price,imageUrl,category}).then((res)=>{
+      //  setdata(res.data)
+       
+    //}).catch((err)=>{
+      //  console.log(err);
+    //})
+   
+//}
+   
+//}
+  //return (
+    //<div className='add'>
+      //<ul>
+        //<h2>Enter the cordination</h2>
+       //<input  type="text" placeholder='name'  onChange={(e)=>{setName(e.target.value)}}/>   
+       //<input type="text" placeholder='description' onChange={(e)=>{setDescription(e.target.value)}}/>
+       //<input type="text" placeholder='price'   onChange={(e)=>{setPrice(e.target.value)}}/>
+       //<input type="text" placeholder='imageUrl'  onChange={(e)=>{setImageurl(e.target.value)}}/>
+       //<input type="text" placeholder='category'  onChange={(e)=>{setCategory(e.target.value)}}/>
+        //<input type="button" value="submit"/>
+
+      //</ul>
+    //</div>
+  //)
+//}
--- front/src/Component/Update.jsx
+++ front/src/Component/Update.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+const Update = ({id}) => {
+  //[namee,setNamee]=useState("")
+ // [description,setDescription]=useState("")
+  //[price,setPrice]=useState("")
+  //[imageUrl,setImageurl]=useState("")
+  //[category,setCategory]=useState("")
 
-const Update = () => {
+  //const submit =()=>{
+    //axios.put("http://localhost:5002/api/product/add/"+id,{namee,description,price,imageUrl,category}).then((res)=>{
+      //  setdata(res.data)
+       
+    //}).catch((err)=>{
+       // console.log(err);
+    //})
+   
+//}
   return (
-    <div>Update</div>
+    <div className='update'>
+      <ul>
+        <h2>Enter the cordination</h2>
+       <input  type="text" placeholder='name' />   
+       <input type="text" placeholder='description'/>
+       <input type="text" placeholder='price'   />
+       <input type="text" placeholder='imageUrl'  />
+       <input type="text" placeholder='category'  />
+        <input type="button" value="submit"/>
+
+      </ul>
+    </div>
   )
 }
 
 export default Update
+ 
+//import React, { useState } from 'react'
+//import axios from 'axios'
+//const Update = ({id}) => {
+  //[namee,setNamee]=useState("")
+ // [description,setDescription]=useState("")
+  //[price,setPrice]=useState("")
+  //[imageUrl,setImageurl]=useState("")
+  //[category,setCategory]=useState("")
+
+  //const submit =()=>{
+    //axios.put("http://localhost:5002/api/product/updtate/"+id,{namee,description,price,imageUrl,category}).then((res)=>{
+      //  setdata(res.data)
+       
+    //}).catch((err)=>{
+       // console.log(err);
+    //})
+   
+//}
+  //return (
+    //<div className='add'>
+      //<ul>
+        //<h2>Enter the cordination</h2>
+       //<input  type="text" placeholder='name'  onChange={(e)=>{setName(e.target.value)}}/>   
+       //<input type="text" placeholder='description' onChange={(e)=>{setDescription(e.target.value)}}/>
+       //<input type="text" placeholder='price'   onChange={(e)=>{setPrice(e.target.value)}}/>
+       //<input type="text" placeholder='imageUrl'  onChange={(e)=>{setImageurl(e.target.value)}}/>
+       //<input type="text" placeholder='category'  onChange={(e)=>{setCategory(e.target.value)}}/>
+        //<input type="button" value="submit"/>
+
+      //</ul>
+    //</div>
+  //)
+//}
